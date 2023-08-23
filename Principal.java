c0565f77808930b2e9f2eb1352c5536b1c467505
vladimir <vladimir.huayllani@ucsm.edu.pe>
2023-08-22 20:42:37 -0500

    cambien la variable vida por heal

--- Principal.java
+++ Principal.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args){
-        int vida, rp;
+        int heal, rp;
         boolean fl = false;
-        vida = 0;
+        heal = 0;
         Scanner scanner = new Scanner(System.in);
         do {
             vida++;
-            System.out.println("Vida: " + vida + "\t¿Cual es tu número? ");
+            System.out.println("Vida: " + heal + "\t¿Cual es tu número? ");
             rp = scanner.nextInt();
             if (rp == 1234){
                 fl = true;
             }
-        } while (vida < 6 && !fl);
+        } while (heal < 6 && !fl);
         if (fl){
             System.out.println("Adivinaste!!!");
         }
